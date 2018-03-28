3b341e0dfe5fbb4b37efa4092340921b52dd5415
Hendry Zheng <hendry@hendryzheng.com>
2018-03-28 17:43:38 +0800

    update name

--- BetstreakFull.sol
+++ BetstreakFull.sol
@@ -112,11 +112,11 @@ contract BST is StandardToken { // CHANGE THIS. Update the contract name.
 
     // This is a constructor function 
     // which means the following function name has to match the contract name declared above
-    function BetstreakToken() {
+    function BST() {
         unitEthWei = 1000000000000000000;
         balances[msg.sender] = 1000000000000000;               // Give the creator all initial tokens. This is set to 1000 for example. If you want your initial tokens to be X and your decimal is 5, set this value to X * 100000. (CHANGE THIS)
         totalSupply = 1000000000000000;                        // Update total supply (1000 for example) (CHANGE THIS)
-        name = "Betstreak Token";                                   // Set the name for display purposes (CHANGE THIS)
+        name = "BST";                                   // Set the name for display purposes (CHANGE THIS)
         decimals = 6;                                               // Amount of decimals for display purposes (CHANGE THIS)
         symbol = "BST";                                             // Set the symbol for display purposes (CHANGE THIS)
         unitsOneEthCanBuy = 5000;                                      // Set the price of your token for the ICO (CHANGE THIS)
